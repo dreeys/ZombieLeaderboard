f9a5ad2427de068dff4c89cb0e90ee0c37e14d4c
Kevin <kevinkula123@hotmail.com>
2025-10-27 18:46:25 +0100

    Enable Swagger in production

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ZombieLeaderboard.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,14 +14,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// ✅ Always enable Swagger (even in Production)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
